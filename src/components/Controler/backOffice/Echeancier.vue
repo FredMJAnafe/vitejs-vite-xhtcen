0b1bb60f6e4f55d2151b6b7c8494255ca47be2e0
FredMJAnafe <122105258+FredMJAnafe@users.noreply.github.com>
2023-03-24 18:14:55 +0100

    Initial commit

---
+++ src/components/Controler/backOffice/Echeancier.vue
@@ -0,0 +1,49 @@
+<template>
+  <span
+    :class="'editable echeance echeance-' + etatEcheance(echeance)"
+    v-for="(echeance, index) in item.echeances"
+    :data-param="index"
+    >{{ _resteAPayer(echeance) }}</span
+  >
+</template>
+
+<script>
+const ECHEANCE_ETAT_INIT = 'initial';
+const ECHEANCE_ETAT_REGLE = 'regle';
+const ECHEANCE_ETAT_RETARD = 'retard';
+const ECHEANCE_ETAT_ENCOURS = 'en_cours';
+const ECHEANCE_DELAI_JOURS_PAIEMENT = 3;
+
+export default {
+  name: 'Echeancier',
+  props: {
+    item: Object,
+  },
+  methods: {
+    _resteAPayer(echeance, nombre = false) {
+      let r =
+        parseFloat(echeance.montantTotal || 0) -
+        parseFloat(echeance.montantRegle || 0);
+      return nombre ? r : r.toFixed(2);
+    },
+    etatEcheance(echeance) {
+      let r = this._resteAPayer(echeance),
+        d = echeance.dateOuverture,
+        dt = new Date(d);
+      if (r == 0) {
+        return ECHEANCE_ETAT_REGLE;
+      }
+      if (dt.getTime() > Date.now) {
+        return ECHEANCE_ETAT_INIT;
+      }
+      if (
+        dt.getTime() + ECHEANCE_DELAI_JOURS_PAIEMENT * 86400000 <
+        Date.now()
+      ) {
+        return ECHEANCE_ETAT_RETARD;
+      }
+      return ECHEANCE_ETAT_ENCOURS;
+    },
+  },
+};
+</script>
---
+++ src/components/Controler/elementsHTML/widget/fenetreMontant.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="card">
+    <div class="cardHead">
+      <h2>{{montantTotal}}</h2>
+      <font-awesome-icon class="fontIcone" icon="fa-money-bill-transfer" />
+    </div>
+    <div class="contenuCard">
+      <small>Montant à solder <strong>{{this.$props.montant.intitule}}</strong></small>
+      <div class="indicateurCard">
+        <div class="affichageIndicateurCard"></div>
+      </div>
+      <div class="inputBoxFacturier">
+      <span class="detailFacturier">Début </span>
+      <input
+        id="dateNaissanceContrat"
+        type="date"
+        name="dateDebut"
+        @input="this.SiContratAvenant"
+        required
+      />
+    </div>
+    <div class="inputBoxFacturier">
+      <span class="detailFacturier">Fin </span>
+      <input
+        id="dateNaissanceContrat"
+        type="date"
+        name="dateFin"
+        @input="this.SiContratAvenant"
+        required
+      />
+    </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default { 
+  name: "fenetreMontant",
+  computed:{
+    montantTotal() {
+      return this.montant.montant;
+    }
+  },
+  props:{
+    montant:Object
+  }
+}
+</script>
+
+<style scoped>
+section.card {
+  max-width:250px;
+  min-width: 170px;
+}
+.cardHead{
+  display: flex;
+  justify-content: space-between;
+  margin-left: 1em;
+  margin-right: 1em;
+  align-items: flex-start;
+}
+.fontIcone{
+  font-size: 2rem;
+  color: var(--color-dark);
+}
+
+.cardHead h2{
+  font-size: 18px;
+  color: #555;
+  line-height:1;
+  margin-top: 8px;
+}
+
+.contenuCard{
+  padding-left: 1rem;
+  color: #555555;
+  font-weight: 400;
+  text-align:left;
+}
+.contenuCard .inputBoxFacturier {
+  text-align:right;
+}
+.contenuCard input{
+  max-width:100px;
+  margin:3px;
+}
+
+</style>
